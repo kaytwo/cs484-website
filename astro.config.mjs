15aeabd3a53b9a3d046cdb8e7aa72f123ab80f12
Chris Kanich <ckanich@uic.edu>
2024-08-28 11:28:02 -0500

    fix today jumper to not jump into the past

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,8 +2,6 @@ import { defineConfig } from "astro/config";
 import starlight from "@astrojs/starlight";
 import tailwind from "@astrojs/tailwind";
 
-import cloudflare from "@astrojs/cloudflare";
-
 // https://astro.build/config
 export default defineConfig({
   integrations: [
