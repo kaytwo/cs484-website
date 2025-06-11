35ed738eb2ec137a2efe7929dd56cd151919c0cd
Chris Kanich <ckanich@uic.edu>
2025-06-10 19:34:34 -0500

    no console logging

--- src/utils/contentUtils.ts
+++ src/utils/contentUtils.ts
@@ -6,7 +6,6 @@ import { courseConfig } from '../courseConfig';
  * Transforms content data by converting relative dates to absolute dates
  */
 export function transformContentData(data: ContentData): ContentData {
-    console.log('Transforming content data:', JSON.stringify(data, null, 2));
     const transformed = { ...data };
 
 
@@ -17,16 +16,11 @@ export function transformContentData(data: ContentData): ContentData {
 
     // Handle homework dates
     if (transformed.relative_release) {
-        console.log('Transforming relative_release:', transformed.relative_release);
         transformed.release = calculateAbsoluteDate(transformed.relative_release);
-        console.log('Transformed to release:', transformed.release);
     }
     if (transformed.relative_due) {
-        console.log('Transforming relative_due:', transformed.relative_due);
         transformed.due = calculateAbsoluteDate(transformed.relative_due);
-        console.log('Transformed to due:', transformed.due);
     }
 
-    console.log('Transformed data:', JSON.stringify(transformed, null, 2));
     return transformed;
 } 
